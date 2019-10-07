6c235b2765a32d10caa74ae6700a0c183933f664
tasneem <tasneem.raneem555@gmail.com>
2019-10-07 15:20:02 +0300

    a

---
+++ java/about-me.js
@@ -0,0 +1,55 @@
+'use strict'
+var love=prompt('do you love me?');
+if ( love=='yes') {
+ alert('of course you do');
+
+} else if (love=='y'){
+    alert('of course you do');
+
+}else{
+    alert('Thank you');
+}
+var dream=prompt('do you have a dream like me?');
+if ( dream=='yes') {
+    alert('of course you do');
+   
+   } else if (love=='y'){
+       alert('of course you do');
+   
+   }else{
+       alert( 'You must have a dream');
+   }
+   var gender=prompt('are you a girl?');
+if ( gender=='yes') {
+    alert('you are like me');
+   
+   } else if (gender=='y'){
+       alert('you are like me');
+   
+   }else{
+       alert( 'You must be boy');
+
+   }
+   var born=prompt('are you born in 1997?');
+   if ( born=='yes') {
+       alert('You are at my age');
+      
+      } else if (born=='y'){
+          alert('You are at my age');
+      
+      }else{
+          alert( 'You must be old or young than me');
+      }
+      var hoppy=prompt('are you playing giutar');
+      if ( hoppy=='yes') {
+          alert('I also play guitar ');
+         
+         } else if (hoppy=='y'){
+             alert('I also play guitar');
+         
+         }else{
+             alert( 'You must be try ,it is fantastic');
+         }
+      
+   
+   
